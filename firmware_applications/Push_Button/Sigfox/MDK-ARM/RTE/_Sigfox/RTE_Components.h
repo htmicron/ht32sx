c601e24020774d036d08a6b99031ef4e2336b0c2
Vilson José Petry Júnior <vilson.petry@htmicron.com.br>
2019-11-19 20:01:25 -0300

    First Push Button Application for iMCP HX32SX Eval Board

---
+++ firmware_applications/Push_Button/Sigfox/MDK-ARM/RTE/_Sigfox/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Sigfox' 
+ * Target:  'Sigfox' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
