4fd60d6c43992111206a96cd7ec8a34268eb6a9d
Hendrick Goncalves <hendrick.goncalves@htmicron.com.br>
2020-01-07 11:13:23 -0200

    Updated microvision configuration and build clean in the project

---
+++ firmware_applications/Monarch_MoveME/MDK-ARM/Sigfox/lps22hb_reg.d
@@ -0,0 +1,4 @@
+sigfox\lps22hb_reg.o: ../Drivers/BSP/Components/lps22hb/lps22hb_reg.c
+sigfox\lps22hb_reg.o: ../Drivers/BSP/Components/lps22hb/lps22hb_reg.h
+sigfox\lps22hb_reg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+sigfox\lps22hb_reg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
---
+++ firmware_applications/Monarch_MoveME/MDK-ARM/Sigfox/lsm6dsl.d
@@ -0,0 +1,6 @@
+sigfox\lsm6dsl.o: ../Drivers/BSP/Components/lsm6dsl/lsm6dsl.c
+sigfox\lsm6dsl.o: ../Drivers/BSP/Components/lsm6dsl/lsm6dsl.h
+sigfox\lsm6dsl.o: ../Drivers/BSP/Components/lsm6dsl/lsm6dsl_reg.h
+sigfox\lsm6dsl.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+sigfox\lsm6dsl.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+sigfox\lsm6dsl.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
