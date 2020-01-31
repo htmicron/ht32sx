27f5342d34325f7f198e03d075ba6a443d0b81af
eduardo.costa <eduardo.costa@htmicron.com.br>
2020-01-31 09:31:20 -0200

    adding bootloader alpha

---
+++ firmware_applications/bootloader/pushButton/MDK-ARM/Sigfox/serial_utils.d
@@ -0,0 +1,4 @@
+sigfox\serial_utils.o: ..\Utils\CLI\src\serial_utils.c
+sigfox\serial_utils.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+sigfox\serial_utils.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+sigfox\serial_utils.o: ..\Utils\CLI\inc\serial_utils.h
